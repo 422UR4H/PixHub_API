dfba26598c6a3dda202c48e8b28e845991611ff8
422UR4H <tiagosindra@id.uff.br>
2024-03-15 21:41:55 -0300

    Fix: add webhook in payment providers

--- Models/PaymentProvider.cs
+++ Models/PaymentProvider.cs
@@ -3,13 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PixHub.Models;
 
-public class PaymentProvider(string token, string bankName)
+public class PaymentProvider(string token, string bankName, string webhook)
 {
   [Key]
   [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
   public int Id { get; set; }
   public string Token { get; set; } = token;
   public string BankName { get; set; } = bankName;
+  public string Webhook { get; set; } = webhook;
 
   public ICollection<PaymentProviderAccount>? PaymentProviderAccounts { get; set; }
 
